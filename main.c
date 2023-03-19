6b9b47a5aab12ff7a2dcc114ba38590a2cbb3570
adilamanm01 <114213304+adilamanm01@users.noreply.github.com>
2023-03-19 00:07:58 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,68 +1,112 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<pthread.h>
+#include<semaphore.h>
 
-#include <stdio.h>
-#include <pthread.h>
-#include <semaphore.h>
-#include <unistd.h>
+int nmax=14;
+int counter=0;
+int readmax=250000000;
+int flag=0;
 
-#define BUF_SIZE 20
-#define NUM_ITEMS 30
+sem_t mutex,wrt;
 
-int buffer[BUF_SIZE];
-int sum = 0;
-int in = 0, out = 0;
-sem_t full, empty;
-pthread_mutex_t mutex;
+pthread_t readers[nmax],writer[nmax];
 
-void *producer(void *arg) {
-  for (int i = 0; i < NUM_ITEMS; i++) {
+void* relax()
+{
+    int i;
+    for(i=0;i<250000000;i++)
+    {
+        i=i;
+    }
+}
+
+void* writer_thread(void* arg)
+{
+    int i=0;
+    flag=1;
+    sem_wait(&wrt);
+    while(i<25000)
+    {
+        counter=counter+1;
+    }
+    printf("writer done\n");
+    flag=0;
+    sem_post(&wrt);
+    return NULL;
+}
+
+void* reader_thread(void* arg)
+{
     int *x=(int *) arg;
     int y=*x;
-    sem_wait(&empty);
-    pthread_mutex_lock(&mutex);
-    buffer[in] = i + 1;
-    printf("(producer id) i:%d, p%d, buff[%d]:%d, in:%d\n",i,y,in,buffer[in],in);
-    in = (in + 1) % BUF_SIZE;
-    printf("in value changed to:%d\n\n",in);
-    pthread_mutex_unlock(&mutex);
-    sem_post(&full);
-  }
-  return NULL;
+
+    sem_wait(&mutex);
+    if(flag==1)
+    {
+        printf("error\n");
+    }
+    else
+    {
+        int temp=counter;
+        printf("reader done :%d\n",temp);
+    }
+    sem_post(&mutex);
+    relax();
+    return NULL;
 }
 
-void *consumer(void *arg) {
-  int item;
-  for (int i = 0; i < NUM_ITEMS; i++) {
-    sem_wait(&full);
-    pthread_mutex_lock(&mutex);
-    //printf("i value(consumer id):%d\n",i);
-    item = buffer[out];
-    out = (out + 1) % BUF_SIZE;
-    pthread_mutex_unlock(&mutex);
-    sem_post(&empty);
-    //printf("sum= %d+%d\n",sum,item);
-    sum += item;
-    //printf("sum value:%d\n",sum);
-  }
-  printf("\n\n");
-  return NULL;
-}
+int main(int argc,char* argv[])
+{
+    int i,k,n;
+    int attr[10000];
+    n=atoi(argv[1]);
+    //printf("%d\n",n);
+    k = (int) (n/2);
+    //printf("%d\n",n);
 
-int main() {
-  pthread_t t1, t2, t3, t4;
-  int a[10];
-  a[0]=1;
-  a[1]=2;
-  sem_init(&full, 0, 0);
-  sem_init(&empty, 0, BUF_SIZE);
-  pthread_mutex_init(&mutex, NULL);
-  pthread_create(&t1, NULL, producer, (void *) &a[0]);
-  pthread_create(&t2, NULL, producer, (void *) &a[1]);
-  pthread_create(&t3, NULL, consumer, (void *) &a[0]);
-  pthread_create(&t4, NULL, consumer, (void *) &a[1]);
-  pthread_join(t1, NULL);
-  pthread_join(t2, NULL);
-  pthread_join(t3, NULL);
-  pthread_join(t4, NULL);
-  printf("The value of SUM is: %d\n", sum);
-  return 0;
+    if(argc==2)
+    {
+        //printf("okay\n");
+        if(n>0 && n<15)
+        {
+            //printf("okay2\n");
+            for(i=0;i<k;i++)
+            {
+                for(i = 0; i < k; i++)
+                    {
+                        //printf("%d\n",i);
+                        pthread_create(&readers[i], &attr[0], reader_thread,(void*) i);
+                    }
+
+                    for(i = 0; i < k; i++)
+                    {
+                        //printf("%d\n",i);
+                        pthread_join(&readers[i],NULL);
+                    }
+
+                    pthread_create(&writer[0], &attr[0], writer_thread,NULL);
+                    pthread_join(&writer[0],NULL);
+
+                    for(i = k ; i < n ; i++)
+                        {
+                            pthread_create(&readers[i], &attr[0], reader_thread,(void*) i);
+                        }
+                    for(i = k ; i < n ; i++)
+                        {
+                            pthread_join(&readers[i], NULL);
+                        }
+
+
+            }
+        }
+        else{
+            printf("invalid : n should be more than 0 and less than 15\n");
+        }
+    }
+    else{
+        printf("enter valid input\n");
+    }
+
+    return 0;
 }
